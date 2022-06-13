f3f228b42d9507db2444af1dc6c106ea6e4d4b7b
Lmy20020802 <105573171+Lmy20020802@users.noreply.github.com>
2022-06-13 17:26:06 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author lmy
+ * @Date 2022/6/13 15:09
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("lmy");
+    }
+}
